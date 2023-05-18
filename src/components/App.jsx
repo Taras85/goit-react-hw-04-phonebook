bf7f6a1c8f271331d6c79c39f254a7fbdfde8e5c
Taras <topanasenko85@gmail.com>
2023-05-18 19:03:21 +0300

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,79 @@
-import { Component } from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
+import { Component, useEffect, useState } from 'react';
+import {ContactForm}  from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import s from './App.module.css';
+import { nanoid } from 'nanoid';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
+
+
+const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? defaultContacts
+  );
+
+
+useEffect(() => {
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+}, [contacts]);
+
+const addContact = (name, number) => {
+  const contact = {
+    id: nanoid(),
+    name,
+    number,
   };
+  contacts.some(
+    i =>
+      i.name.toLowerCase() === contact.name.toLowerCase() ||
+      i.number === contact.number
+  )
+    ? alert(`${name} is already in contacts`)
+    : setContacts(prevContacts => [contact, ...prevContacts]);
+};
 
-  addContact = contact => {
-    if (this.state.contacts.some(item => (item.name).toLowerCase()  === (contact.name).toLowerCase())) {
-     
-      alert(`${contact.name} is already in contacts`);
-    } else {
-      this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
-    }
-  };
+const onChangeFilter = e => {
+  setFilter(e.target.value);
+};
 
-  onDeleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
+const filteredContacts = () => {
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+};
 
-  onChangeFilter = e => {
-    this.setState({ filter: e.target.value });
-  };
-
-
-  componentDidMount(){
-    const getContact=localStorage.getItem('contact')
-    const parsetContacts= JSON.parse(getContact);
-    if(parsetContacts){
-    this.setState({contacts: parsetContacts})
-    }
-  }
-  
-  componentDidUpdate(prevProps, prevState ){
- if (this.state.contacts !== prevState.contacts){
-  localStorage.setItem('contact', JSON.stringify(this.state.contacts))
-    }
-  }
-
-
-  render() {
-    const filteredContacts = this.state.contacts.filter(contact =>
-      contact.name
-        .toLowerCase()
-        .trim()
-        .includes(this.state.filter.toLowerCase())
-    );
+const onDeleteContact = id => {
+  setContacts(prevContacts =>
+    prevContacts.filter(contact => contact.id !== id)
+  );
+};
 
     return (
       <div className={s.appContainer}>
         <h1 className={s.titlePhonebook}>Phonebook</h1>
 
-        <ContactForm onSubmit={this.addContact} />
+        <ContactForm onSubmit={addContact} />
 
         <h2 className={s.titlePhonebook}>Contacts</h2>
         <Filter
-          onChangeFilter={this.onChangeFilter}
-          value={this.state.filter}
+          onChangeFilter={onChangeFilter}
+          value={filter}
         />
         <ContactList
           // id={this.id}
-          onDeleteContact={this.onDeleteContact}
-          contacts={filteredContacts}
+          onDeleteContact={onDeleteContact}
+          contacts={filteredContacts()}
         />
       </div>
     );
-  }
+
 }
+
+export default App;
